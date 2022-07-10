dec701d3358730fdb639ee3213a9461f803fe726
HanifVW <ianhanif66@gmail.com>
2022-07-10 17:23:11 +0700

    new file for main2

---
+++ lib/model_Dummy.dart
@@ -0,0 +1,16 @@
+class User {
+  final String id;
+  final String nama;
+  final String username;
+  final String password;
+
+ const User({
+    required this.id,
+    required this.nama,
+    required this.username,
+    required this.password,
+    
+  });
+  
+}
+
